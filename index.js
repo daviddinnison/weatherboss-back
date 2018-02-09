7c7aca864b997bf4a972e9f8199045205a1a6e2c
daviddinnison <david.g.dinnison@gmail.com>
2018-02-08 21:27:28 -0500

    heroku attempt 1

--- index.js
+++ index.js
@@ -1,44 +1,116 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+"use strict";
+require("dotenv").config();
 
-const {PORT, CLIENT_ORIGIN} = require('./config');
-const {dbConnect} = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const passport = require("passport");
+const { PORT, CLIENT_ORIGIN, DATABASE_URL } = require("./config");
+
+const { User } = require("./users/models");
+const { router: usersRouter } = require("./users");
+const { router: authRouter, localStrategy, jwtStrategy } = require("./auth");
 
 const app = express();
 
 app.use(
-    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
-    })
+  morgan(process.env.NODE_ENV === "production" ? "common" : "dev", {
+    skip: (req, res) => process.env.NODE_ENV === "test"
+  })
 );
 
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  if (req.method === "OPTIONS") {
+    return res.send(204);
+  }
+  next();
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use("/api/users/", usersRouter);
+app.use("/api/auth/", authRouter);
+
+const jwtAuth = passport.authenticate("jwt", { session: false });
+
+// A protected endpoint which needs a valid JWT to access it
+app.get("/api/protected/:id", jwtAuth, (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        return res.status(404).end();
+      }
+      return res.status(200).json(user.locations);
     })
-);
+    .catch(err => next(err));
+});
 
-app.get('/api',(req, res) => {
-    res.json({name: 'David'})
-})
+app.use("*", (req, res) => {
+  return res.status(404).json({ message: "Not Found" });
+});
 
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
+let server;
 
-function runServer(port = PORT) {
-    const server = app
-        .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(PORT, () => {
+          console.log(`Your app is listening on port ${PORT}`);
+          resolve();
         })
-        .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
+        .on("error", err => {
+          mongoose.disconnect();
+          reject(err);
         });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log("Closing server");
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 }
 
 if (require.main === module) {
-    dbConnect();
-    runServer();
+  runServer().catch(err => console.error(err));
 }
 
-module.exports = {app};
+module.exports = { app, runServer, closeServer };
+
+// function runServer(port = PORT) {
+//   const server = app
+//     .listen(port, () => {
+//       console.info(`App listening on port ${server.address().port}`);
+//     })
+//     .on("error", err => {
+//       console.error("Express failed to start");
+//       console.error(err);
+//     });
+// }
+
+// if (require.main === module) {
+//   dbConnect();
+//   runServer();
+// }
+
+// module.exports = { app };
